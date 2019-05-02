e81114ef746db9910a81ec5e7d167bfb28bb0294
Ankit Malik <malik.ankit@indiamart.com>
2019-05-02 12:38:59 +0530

    adding first

---
+++ src/operators.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("ankit")
+
+	fmt.Println("1+1", 1+1)
+	fmt.Println("7.0/3.0", 7.0/3.0)
+
+	fmt.Println("7.0/3", 7.0/3)
+
+	fmt.Println(true && false)
+	fmt.Println(false || true)
+	fmt.Println(!true)
+
+}
