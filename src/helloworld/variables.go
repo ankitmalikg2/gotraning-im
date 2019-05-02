40e5d66712ba8bf7bc9de35a052f376143e89b94
Ankit Malik <malik.ankit@indiamart.com>
2019-05-02 12:44:22 +0530

    adding changes

---
+++ src/helloworld/variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello ankit malik")
+
+	var a = "ankit"
+	fmt.Println(a)
+
+	var b, c int = 2, 4
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+}
